253aacd0239870131199bbc6c261404599e0a315
Barry Anderson <barryanderson@users.noreply.github.com>
2019-04-15 14:12:59 +0100

    Added initial graphql structure and tested

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp= require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.json())  // Json body parser middleware.
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!')
-})
+// GraphQL endpoint.
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    // Resolvers.
+    events: () => {
+      return ['JS Club', 'All night party', 'Cooking 101']
+    },
+    createEvent: (args) => {
+      const eventName = args.name
+      return eventName
+    }
+  },
+  graphiql: true // Enable graphiql front end for development.
+}))
 
 app.listen(3000)
